ae7722bde76db2ad5f5273ef7759be32985ce39a
Thanh Thien <thienqnpro02@gmail.com>
2019-09-07 16:40:26 +0700

    fist change of project1

--- LamQuenGit/Project1/Form1.cs
+++ LamQuenGit/Project1/Form1.cs
@@ -16,5 +16,10 @@ namespace Project1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("HI");
+        }
     }
 }
